933e8bfebd3a928004c3213efc64310f8f4b7621
Kim <kimly.jet@gmail.com>
2022-09-07 12:25:08 +0900

    Feat: Setup Tailwind and Flowbite

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+     <button className="bg-blue-500">Submit</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar

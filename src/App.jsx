48e234c4fb925daeac941b5a9b4528c5ffda6d8f
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-09-07 16:54:40 +0900

    Feat: Add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Login from "./components/Login";
+import Landing from "./components/Landing";
 
 function App() {
   return (

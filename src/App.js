7c87907352868f027a18c4d9f3f911365a5d0f6a
felipemfm <felipemfm@gmail.com>
2022-09-07 13:59:44 +0900

    Feat: Add React-Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div>
-     <button className="bg-blue-500">Submit</button>
+      <button className="bg-blue-500">Submit</button>
+      <Router>
+        <Routes>
+          <Route exact="/" />
+          <Route exact="/login" />
+          <Route exact="/registration" />
+          <Route exact="/footprint" />
+          <Route exact="/results" />
+          <Route exact="/tiers" />
+          <Route exact="/tiers" />
+          <Route exact="/payment" />
+          <Route exact="/confirmation" />
+        </Routes>
+      </Router>
     </div>
   );
 }

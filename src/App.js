472c9fbb00d46f5baa0f2c34ce2ae541fd4f65ea
Kim <kimly.jet@gmail.com>
2022-09-07 16:31:02 +0900

    Fix: Fix NavBar name component spelling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Login from "./components/Login";
 
 function App() {

81e623898012251f61db786e8431d42b9740528e
Kim <kimly.jet@gmail.com>
2022-09-08 19:42:23 +0900

    Feat/Refactor: Get results from Footprint selection"
    Co-authored-by: 91Pudge <91Pudge@users.noreply.github.com>

--- src/components/FootprintCard.jsx
+++ src/components/FootprintCard.jsx
@@ -1,27 +1,55 @@
-import { Card, Button } from "flowbite-react";
+import { Card, Button, Accordion, Tooltip } from "flowbite-react";
 
-const FootprintCard = ({ title, description, buttonText }) => {
+const FootprintCard = ({
+  title,
+  grocery,
+  takeOut,
+  petBottles,
+  disposables,
+  setResult,
+  change,
+}) => {
+  function getResult() {
+    setResult(title);
+    change(true);
+  }
+
   return (
-    <div className="max-w-sm">
+    <div className="transition ease-out duration-300 hover:-translate-y-1 hover:scale-105">
       <Card
+        horizontal={true}
         imgAlt="Meaningful alt text for an image that is not purely decorative"
         imgSrc="https://media.timeout.com/images/105656638/image.jpg"
       >
-        <h5 className="text-2xl font-bold tracking-tight text-center text-gray-900 dark:text-white">
+        <h3 className="text-2xl font-bold tracking-tight text-center text-gray-900 dark:text-white">
           {title}
-        </h5>
-        <div className="h-40">
+        </h3>
+
+        <div className="flex flex-col gap-2 h-96">
+          <h4 className="font-medium">Grocery Store</h4>
           <p className="font-normal text-gray-700 dark:text-gray-400">
-            {description}
+            {grocery}
+          </p>
+          <h4 className="font-medium">Take-out</h4>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            {takeOut}
+          </p>
+          <h4 className="font-medium">PET bottles</h4>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            {petBottles}
+          </p>
+          <h4 className="font-medium">Disposables</h4>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            {disposables}
           </p>
         </div>
 
-        <a
-          href="#"
-          class="inline-flex items-center py-2 px-3 text-sm font-medium justify-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        <button
+          className="inline-flex items-center mt-4 py-2 px-3 text-sm font-medium justify-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={getResult}
         >
-          {buttonText}
-        </a>
+          Select
+        </button>
       </Card>
     </div>
   );
--- src/pages/Footprint.jsx
+++ src/pages/Footprint.jsx
@@ -1,56 +1,84 @@
 import FootprintCard from "../components/FootprintCard";
+import { useState, useEffect } from "react";
+import Results from "../components/Results";
+import Carrousel from "../components/Carrousel";
+import { Button } from "flowbite-react";
 
 const Footprint = () => {
+  const [result, setResult] = useState("");
+  const [change, setChange] = useState(false);
+
+  const results = document.querySelector("#results");
+
+  const plasticFootprintEstimation = [
+    {
+      title: "🐕 Small",
+      grocery:
+        "I grow most of my own vegetables, or primarily buy packaging-free fruits & vegetables",
+      takeOut: "I cook at home most meals, and rarely order take-out",
+      petBottles: "I very rarely use drink vending machines",
+      disposables:
+        "I make an effort to avoid plastic packaging on the items I choose from the supermarket",
+    },
+    {
+      title: "🐎 Medium",
+      grocery:
+        "I try to avoid single-serving sizes and buy bulk-size and minimally packaged products whenever possible.",
+      takeOut: "I order takeout or delivery 1-2 times per month or less. ",
+      petBottles: "I buy no more than 1-2 drinks in PET bottles per month.",
+      disposables:
+        "I carry reusable bags, water bottle/cup, cutlery, straw, etc. to avoid disposables.",
+    },
+    {
+      title: "🐘 High",
+      grocery:
+        "I appreciate that my supermarket thoroughly packages foods in plastic for freshness and sanitation",
+      takeOut:
+        "I order delivery (such as Uber Eats) 1-2 times per week or more",
+      petBottles:
+        "I drink 1-2 (or more) PET bottle drinks per week from vending machines, convenience stores, etc.",
+      disposables:
+        "I often receive a plastic bag and/or disposable utensils at the cash register",
+    },
+  ];
+
+  useEffect(() => {}, [change]);
+
   return (
-    <div className="flex flex-grow flex-col justify-center items-center gap-6">
-      <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center">
-        What is your 
-        <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-          {" "}plastic footprint
-        </span>?
-      </h1>
+    <div className="flex flex-col flex-grow mt-8 items-center gap-6 md:items-center md:justify-center md:mt-0 md:gap-10">
+      {!result ? (
+        <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center">
+          What is your <span> </span>
+          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            plastic footprint
+          </span>
+          ?
+        </h1>
+      ) : (
+        <Results result={result} />
+      )}
 
       {/* Footprint cards container */}
-      <div className=" flex gap-14 justify-center items-center">
-        <FootprintCard
-          title={"🐕 Small consumption"}
-          description={
-            <ul>
-              <li>I rarely eat out and often cook my own meals</li>
-              <li>I always use a re-usable bag</li>
-              <li>I always use a re-usable bottle for drinks</li>
-            </ul>
-          }
-          buttonText={"Select"}
-        />
-        <FootprintCard
-          title={"🐎 Medium consumption"}
-          description={
-            <ul>
-              <li>I sometimes cook at home</li>
-              <li>
-                I own a re-usable bag but sometimes forget to bring it with me
-              </li>
-              <li>
-                I use a re-usable bottle for water/coffee/tea, but rely on
-                plastic cups/straws when buying drinks at cafes
-              </li>
-            </ul>
-          }
-          buttonText={"Select"}
-        />
-        <FootprintCard
-          title={"🐘 Large consumption"}
-          description={
-            <ul>
-              <li>I often purchase convenience foods almost every day</li>
-              <li>I don't own a re-usable bag. I purchase plastic bags</li>
-              <li>I don't own a re-usable bottle</li>
-            </ul>
-          }
-          buttonText={"Select"}
-        />
-      </div>
+      {!result ? (
+        <div className=" flex flex-col gap-8 w-3/4 justify-center items-center md:flex-row md:gap-14 md:w-full">
+          {plasticFootprintEstimation.map((card) => {
+            return (
+              <FootprintCard
+                title={card.title}
+                grocery={card.grocery}
+                takeOut={card.takeOut}
+                petBottles={card.petBottles}
+                disposables={card.disposables}
+                buttonText={"Select"}
+                setResult={setResult}
+                change={setChange}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <Button size={"xl"}>Start offsetting</Button>
+      )}
     </div>
   );
 };

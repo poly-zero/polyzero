4614951e1531a392073f1d21ab7df802e89d18fa
Kim <kimly.jet@gmail.com>
2022-09-09 13:39:13 +0900

    Feat: Add OrderConfirmation page

--- src/components/TierCard.jsx
+++ src/components/TierCard.jsx
@@ -1,6 +1,19 @@
 import { Card } from "flowbite-react";
+import { useNavigate } from "react-router-dom";
 
-const TierCard = ({ title, description, cost, image }) => {
+const TierCard = ({ title, description, cost, image, setTier }) => {
+  const navigateTo = useNavigate();
+
+  function assignTier() {
+    setTier({
+      image: image,
+      title: title,
+      description: description,
+      cost: cost,
+    });
+    navigateTo("/confirmation");
+  }
+
   return (
     <div className="transition ease-out duration-300 hover:-translate-y-1 hover:scale-105">
       <Card imgSrc={image} imgAlt={""}>
@@ -13,12 +26,12 @@ const TierCard = ({ title, description, cost, image }) => {
         <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {cost}
         </h3>
-        <a
-          href="/"
+        <button
           class="inline-flex items-center py-2 px-3 text-sm font-medium justify-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={assignTier}
         >
           Select
-        </a>
+        </button>
       </Card>
     </div>
   );
---
+++ src/pages/OrderConfirmation.jsx
@@ -0,0 +1,65 @@
+import { Card } from "flowbite-react";
+import { ReactComponent as FaceBook } from "../assets/socialMediaIcons/icons8-facebook.svg";
+import { ReactComponent as Instagram } from "../assets/socialMediaIcons/icons8-instagram.svg";
+import { ReactComponent as LinkedIn } from "../assets/socialMediaIcons/icons8-linkedin.svg";
+import { ReactComponent as Twitter } from "../assets/socialMediaIcons/icons8-twitter.svg";
+import { ReactComponent as Line } from "../assets/socialMediaIcons/icons8-line.svg";
+
+const OrderConfirmation = ({ tier }) => {
+  return (
+    <div className="flex flex-col flex-grow items-center justify-center">
+      <div className="flex flex-col items-center w-1/2">
+        <h1 class="text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-center">
+          Thank you for supporting,
+          <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+            {" "}
+            Zach
+          </span>
+          !
+        </h1>
+        <p className="w-1/2 my-8 text-xl font-normal text-center text-gray-500 dark:text-gray-400">
+          Here at PolyZero we focus on markets where technology, innovation, and
+          capital can unlock long-term value and drive economic growth.
+        </p>
+      </div>
+
+      <div className="flex rounded-lg bg-slate-200 items-center px-20 py-8 gap-24">
+        <div className="max-w-xs">
+          <Card imgSrc={tier.image}>
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {tier.title}
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {tier.description}
+            </p>
+            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {tier.cost}
+            </h5>
+          </Card>
+        </div>
+        <div className="flex flex-col items-center w-1/2">
+          <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 dark:text-white">
+            Share what you did:
+          </h2>
+          <div className="flex gap-4">
+            <FaceBook />
+            <Instagram />
+            <Twitter />
+            <LinkedIn />
+            <Line />
+          </div>
+
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+              Learn more
+            </p>
+            <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 dark:text-white">
+            Learn more
+          </h2>
+          <div className="flex gap-4"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderConfirmation;

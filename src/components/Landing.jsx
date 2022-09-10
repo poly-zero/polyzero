5207265fa0807d543c5530457fef59081567e09a
91Pudge <73273137+91Pudge@users.noreply.github.com>
2022-09-10 18:34:09 +0900

    Style: Change to style of  landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -28,7 +28,7 @@ const Landing = () => {
       <div className="flex-col   items-center mt-11 align-items ">
         <div>
           <img
-            className="flex-col items-center mx-auto"
+            className="flex-col items-center mx-auto  p-4"
             alt="Pictue of river running through jungle"
             src="https://siwi.org/wp-content/uploads/2021/07/why-water_topics-present-at-launch_rivers2.jpg"
             width="500"
@@ -39,61 +39,71 @@ const Landing = () => {
           PolyZero
         </h1>
         <p className=" mx-auto  flex justify-center  text-1xl gap-8 mt-11">
-          A way to help the planet.
+          A better way to help the planet.
         </p>
         <button
           onClick={() => navigateTo("/footprint")}
-          className=" flex mx-auto bg-orange-400 mb-4 mt-5  rounded p-2  ring-1 ring-black "
+          className=" flex mx-auto bg-orange-400 mb-4 mt-5  rounded p-2  ring-1 ring-black shadow-10xl"
           type="button"
         >
           Get started{" "}
         </button>
       </div>
-      <div className="flex-col">
+      <div className="flex-col shadow-xl">
         {" "}
-        <div className="flex gap-4">
-          <h6 className="font-extrabold">Did you know:</h6>
-          <br />
-          <p className="">{randomFact}</p>
+        <div class=" flex flex-col justify-center items-center">
+          <h1 className="text-2xl mb-11 underline font-light">Did you know</h1>
         </div>
-        <div className="flex gap-4">
-          <article onClick={copyToClipboard}>
-            <button className="flex justify-end">
-              <a
-                href={
-                  `https://twitter.com/intent/tweet?text=${randomFact}` +
-                  ` https://polyzero.earth/`
-                }
-                class="twitter-share-button"
-                data-size="large"
-                data-hashtags="kanjifyed"
-                data-show-count="false"
-              >
-                <Twitter />
-              </a>
-              <a
-                href={`https://www.linkedin.com/sharing/share-offsite/?url=https://polyzero.earth/`}
-                class="twitter-share-button"
-                data-size="large"
-                data-hashtags="kanjifyed"
-                data-show-count="false"
-              >
-                <LinkedIn />
-              </a>
-              <a
-                href={`https://www.facebook.com/sharer/sharer.php?u=https://polyzero.earth/`}
-                class="twitter-share-button"
-                data-size="large"
-                data-hashtags="kanjifyed"
-                data-show-count="false"
-              >
-                <FaceBook />
-              </a>
-            </button>
-          </article>
+        <div className="flex flex-col justify-center items-center text-center font-bold  ">
+          <div className="max-w-xs">
+            Single-use plastics, which account for half of the plastic we use
+            each year, have an average useful life of 12 to 15 minutes and yet
+            can take up to 500 years to break down.
+          </div>
+          <a
+            href="https://www.iberdrola.com/sustainability/how-to-reduce-plastic-use"
+            className="text-blue-600 hover:text-blue-800 text-center justify-center items-center font-light"
+          >
+            Click here for the full report by LOOP (Life Out Of Plastic)
+          </a>
+        </div>
+        <div className="flex flex-col justify-left justify-center items-center  text-center mt-5 font-bold">
+          <div class=" max-w-xs">
+            The carbon footprint of just five plastic bags is the equvialant of
+            1kg of CO2e.
+            <p className="text-center">Source:</p>
+            <a
+              href="https://timeforchange.org/plastic-bags-and-plastic-bottles-co2-emissions-during-their-lifetime/"
+              className="text-blue-600 hover:text-blue-800 text-center font-light"
+            >
+              Click here for the full report
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-col justify-left items-center text-center mt-5 font-bold ">
+          <div class=" max-w-xs">
+            Japan is the world's second largest per capita consumer of plastic
+            packaging.
+            <p className="text-center">
+              Source: United Nations Environment Programme, 2018
+            </p>
+            <a
+              href="https://www.unep.org/ietc/ja/node/53"
+              className="text-blue-600 hover:text-blue-800 text-center font-light "
+            >
+              Click here for the full report
+            </a>
+          </div>
+        </div>
+        <div class=" flex flex-col  items-end">
+          <div class="">4</div>
+        </div>
+        <div class=" flex flex-col  items-center">
+          <div class="">5</div>
         </div>
       </div>
     </div>
+    // </div>
   );
 };
 

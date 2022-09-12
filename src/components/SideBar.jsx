97d30e1963ee974e75748439ed57f0fb6423cd71
Kim <kimly.jet@gmail.com>
2022-09-12 09:04:38 +0900

    Feat: Add social media buttons to SideBar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,6 +3,11 @@ import { NavLink, useLocation } from "react-router-dom";
 import NavBar from "./NavBar";
 import { logout, auth, getUserInfo } from "../firebase/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { ReactComponent as FaceBook } from "../assets/socialMediaIcons/icons8-facebook.svg";
+import { ReactComponent as Instagram } from "../assets/socialMediaIcons/icons8-instagram.svg";
+import { ReactComponent as LinkedIn } from "../assets/socialMediaIcons/icons8-linkedin.svg";
+import { ReactComponent as Twitter } from "../assets/socialMediaIcons/icons8-twitter.svg";
+import { ReactComponent as Line } from "../assets/socialMediaIcons/icons8-line.svg";
 
 const SideBar = () => {
   const [showSidebar, setShowSidebar] = useState("-left-64");
@@ -28,8 +33,15 @@ const SideBar = () => {
         className={`h-screen fixed top-0 md:left-0 ${showSidebar} flex-row flex-nowrap overflow-hidden shadow-xl bg-white w-64 z-10 py-4 px-6 transition-all duration-300`}
       >
         <div className="flex-col items-stretch min-h-full flex-nowrap px-0 relative">
-          {/* Logo */}
-          <a href="/" rel="noreferrer" className="flex mt-2">
+          {/* Close button for SideBar */}
+          <button
+            className=" float-right md:hidden"
+            onClick={() => setShowSidebar("-left-64")}
+            >
+            X
+          </button>
+            {/* Logo */}
+          <NavLink to="/" exact={true} className="flex mt-2">
             <img
               src="https://raw.githubusercontent.com/poly-zero/polyzero/main/public/images/favicon64.ico"
               className="mr-3 h-14"
@@ -38,10 +50,11 @@ const SideBar = () => {
             <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
               PolyZero
             </span>
-          </a>
+          </NavLink>
+          
           <div className="flex flex-col">
             <hr className="my-4 min-w-full" />
-            
+
             {/* Beginning of nav list */}
             <ul className="flex-col min-w-full flex list-none">
               <li className="rounded-lg mb-4">
@@ -89,48 +102,48 @@ const SideBar = () => {
               {!user && (
                 <>
                   <li className="rounded-lg mb-4">
-                    <a
-                      href="/login"
-                      rel="noreferrer"
+                    <NavLink
+                      to="/login"
+                      exact
                       className={
                         pathName === "/login"
                           ? "flex items-center gap-4 text-sm font-semibold px-4 py-3 rounded-lg bg-gradient-to-r to-emerald-600 from-sky-400 text-white shadow-md"
                           : "flex items-center gap-4 text-sm text-gray-700 font-light px-4 py-3 rounded-lg "
                       }
                     >
-                      {/* <Icon name="fingerprint" size="2xl" /> */}
-                      Login
-                    </a>
+                      {/* <Icon name="dashboard" size="2xl" /> */}
+                      Log in
+                    </NavLink>
                   </li>
                   <li className="rounded-lg mb-4">
-                    <a
-                      href="/registration"
-                      rel="noreferrer"
+                    <NavLink
+                      to="/registration"
+                      exact
                       className={
                         pathName === "/registration"
                           ? "flex items-center gap-4 text-sm font-semibold px-4 py-3 rounded-lg bg-gradient-to-r to-emerald-600 from-sky-400 text-white shadow-md"
                           : "flex items-center gap-4 text-sm text-gray-700 font-light px-4 py-3 rounded-lg "
                       }
                     >
-                      {/* <Icon name="list_alt" size="2xl" /> */}
+                      {/* <Icon name="dashboard" size="2xl" /> */}
                       Register
-                    </a>
+                    </NavLink>
                   </li>
                 </>
               )}
               <li className="rounded-lg mb-4">
-                <a
-                  href="/"
-                  rel="noreferrer"
+                <NavLink
+                  to="/dashboard"
+                  exact
                   className={
                     pathName === "/dashboard"
                       ? "flex items-center gap-4 text-sm font-semibold px-4 py-3 rounded-lg bg-gradient-to-r to-emerald-600 from-sky-400 text-white shadow-md"
                       : "flex items-center gap-4 text-sm text-gray-700 font-light px-4 py-3 rounded-lg "
                   }
                 >
-                  {/* <Icon name="account_circle" size="2xl" /> */}
+                  {/* <Icon name="dashboard" size="2xl" /> */}
                   Dashboard
-                </a>
+                </NavLink>
               </li>
             </ul>
 
@@ -143,6 +156,20 @@ const SideBar = () => {
                 >
                   Share
                 </a>
+              </li>
+              <li>
+                <div className="grid grid-cols-3">
+                  <FaceBook />
+                  <Instagram />
+                  <a
+                    href="https://twitter.com/intent/tweet?text=Checkout PolyZero!"
+                    data-show-count="false"
+                  >
+                    <Twitter />
+                  </a>
+                  <LinkedIn />
+                  <Line />
+                </div>
               </li>
             </ul>
           </div>

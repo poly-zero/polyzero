0e47c8032c21dbd043731afd0958cd6348e0f3b1
Ishaan Bedi <bediishaan6@gmail.com>
2022-09-18 17:09:28 +0530

    fixed issue #149: Back to top button

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    
     <Router>
       <App />
     </Router>

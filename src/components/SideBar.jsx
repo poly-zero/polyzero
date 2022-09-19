17c9e70e8c3a4ef8bd4d50870168c680164018ae
Kim <kimly.jet@gmail.com>
2022-09-20 08:08:50 +0900

    Fix: Renavigate SideBar button

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,11 +8,11 @@ import { useAuthState } from "react-firebase-hooks/auth";
 // import { ReactComponent as LinkedIn } from "../assets/socialMediaIcons/icons8-linkedin.svg";
 // import { ReactComponent as Twitter } from "../assets/socialMediaIcons/icons8-twitter.svg";
 // import { ReactComponent as Line } from "../assets/socialMediaIcons/icons8-line.svg";
-import { ReactComponent as Footprint } from "../assets/navIcons/carbon-footprint.svg";
 import { ReactComponent as Tiers } from "../assets/navIcons/tiers.svg";
 import { ReactComponent as Dashboard } from "../assets/navIcons/dashboard.svg";
 import { ReactComponent as Logout } from "../assets/navIcons/logout.svg";
 import { Progress } from "flowbite-react";
+import FootprintWizard from "../pages/FootprintWizard";
 
 const SideBar = ({ result }) => {
   const [showSidebar, setShowSidebar] = useState("-left-64");
@@ -82,7 +82,7 @@ const SideBar = ({ result }) => {
             <ul className="flex-col min-w-full flex list-none">
               <li className="rounded-lg mb-4">
                 <NavLink
-                  to="/footprint"
+                  to="/wizard"
                   exact="true"
                   className={
                     pathName === "/wizard"
@@ -91,7 +91,7 @@ const SideBar = ({ result }) => {
                   }
                 >
                   {/* <Icon name="dashboard" size="2xl" /> */}
-                  <Footprint />
+                  <FootprintWizard />
                   Footprint Estimator
                 </NavLink>
               </li>

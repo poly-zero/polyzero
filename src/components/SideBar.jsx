17955c5ce96e6f8b968ffe8b7a226a1432b26000
Kim <kimly.jet@gmail.com>
2022-09-22 13:14:45 +0900

    Refactor: Add state for mobileView and icons

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NavLink, Link, useLocation } from "react-router-dom";
 import NavBar from "./NavBar";
 import { logout, auth } from "../firebase/firebase";
@@ -6,24 +6,37 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { ReactComponent as Footprint } from "../assets/navIcons/carbon-footprint.svg";
 import { ReactComponent as Tiers } from "../assets/navIcons/tiers.svg";
 import { Progress, Dropdown, Avatar, Button } from "flowbite-react";
+import {
+  ArrowLeftOnRectangleIcon,
+  UserPlusIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
 
 const SideBar = ({ result }) => {
   const [showSidebar, setShowSidebar] = useState("-left-64");
   const [userInfo, setUserInfo] = useState(null);
   const [user, loading, error] = useAuthState(auth);
+  const [isMobileView, setIsMobileView] = useState(false);
   const pathName = useLocation().pathname;
+
+  useEffect(() => {
+    if (window.innerWidth < 500) setIsMobileView(true);
+  }, []);
 
   return (
     <>
       <div className="">
         <Progress
+        color="green"
           progress={
             !result
               ? 0
               : pathName === "/wizard"
               ? 25
+              : pathName === "/results"
+              ? 45
               : pathName === "/tiers"
-              ? 50
+              ? 60
               : pathName === "/payment"
               ? 75
               : pathName === "/confirmation"
@@ -33,7 +46,7 @@ const SideBar = ({ result }) => {
           size="sm"
         />
       </div>
-      {window.innerWidth < 500 && (
+      {isMobileView && (
         <NavBar
           showSidebar={showSidebar}
           setShowSidebar={setShowSidebar}
@@ -50,13 +63,13 @@ const SideBar = ({ result }) => {
       <div
         className={`h-screen fixed top-0 z-50 md:left-0 ${showSidebar} flex-row flex-nowrap overflow-hidden shadow-xl bg-white w-64 z-10 py-4 px-6 transition-all duration-300`}
       >
-        <div className="flex-col items-stretch min-h-full flex-nowrap px-0 relative">
+        <div className="relative flex-col items-stretch min-h-full px-0 flex-nowrap">
           {/* Close button for SideBar */}
           <button
-            className=" float-right md:hidden"
+            className="float-right md:hidden"
             onClick={() => setShowSidebar("-left-64")}
           >
-            X
+            <XMarkIcon className="w-6" />
           </button>
           {/* Logo */}
           <NavLink to="/" exact="true" className="flex mt-2">
@@ -65,16 +78,16 @@ const SideBar = ({ result }) => {
               className="mr-3 h-14"
               alt="PolyZero Logo"
             />
-            <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               PolyZero
             </span>
           </NavLink>
 
           <div className="flex flex-col">
-            <hr className="my-4 min-w-full" />
+            <hr className="min-w-full my-4" />
             {/* Beginning of nav list */}
-            <ul className="flex-col min-w-full flex list-none">
-              <li className="rounded-lg mb-4">
+            <ul className="flex flex-col min-w-full list-none">
+              <li className="mb-4 rounded-lg">
                 <NavLink
                   to="/wizard"
                   exact="true"
@@ -90,7 +103,7 @@ const SideBar = ({ result }) => {
                 </NavLink>
               </li>
               {result && (
-                <li className="rounded-lg mb-4">
+                <li className="mb-4 rounded-lg">
                   <NavLink
                     to="/tiers"
                     exact="true"
@@ -107,7 +120,7 @@ const SideBar = ({ result }) => {
                 </li>
               )}
             </ul>
-            <ul className="flex-col min-w-full flex list-none absolute bottom-0">
+            <ul className="absolute bottom-0 flex flex-col items-center min-w-full list-none">
               {user ? (
                 <>
                   <div className="flex items-center gap-2">
@@ -145,10 +158,20 @@ const SideBar = ({ result }) => {
                 <>
                   <Button.Group>
                     <Button color="gray">
-                      <Link to="/login">Log in</Link>
+                      <Link to="/login">
+                        <div className="flex flex-col items-center">
+                          <ArrowLeftOnRectangleIcon className="w-6 text-gray-700" />
+                          <span>Log in</span>
+                        </div>
+                      </Link>
                     </Button>
                     <Button color="gray">
-                      <Link to="/registration">Register</Link>
+                      <Link to="/registration">
+                        <div className="flex flex-col items-center">
+                          <UserPlusIcon className="w-6 text-gray-700" />
+                          <span>Register</span>
+                        </div>
+                      </Link>
                     </Button>
                   </Button.Group>
                 </>

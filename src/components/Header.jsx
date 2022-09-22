12191999d91a4065aec1439b0611dd8cf2f48848
Kim <kimly.jet@gmail.com>
2022-09-22 13:15:42 +0900

    Style: Adjust margins of Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = ({ text, highlightedText, afterHighlighted, caption }) => {
   return (
     <header className="flex flex-col items-center gap-6">
-      <h1 className="flex flex-wrap justify-center my-12 text-4xl font-bold text-center text-gray-800 dark:text-white md:text-5xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
+      <h1 className="flex flex-wrap justify-center my-6 text-4xl font-bold text-center text-gray-800 md:my-6 lg:my-8 dark:text-white md:text-5xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
         {text}&nbsp;
         <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-500">
           {highlightedText}

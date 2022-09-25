a7707d07954ec224c0479e532db84a8ad07963c9
Kim <kimly.jet@gmail.com>
2022-09-25 14:57:53 +0900

    Feat: Add caption2 prop to Header component
    - adjust width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,27 @@
-const Header = ({ text, highlightedText, afterHighlighted, caption, caption2 }) => {
+const Header = ({
+  text,
+  highlightedText,
+  afterHighlighted,
+  caption,
+  caption2,
+}) => {
   return (
-    <header className="flex flex-col items-center gap-6">
-      <h1 className="flex flex-wrap justify-center my-6 text-4xl font-bold text-center text-gray-800 md:my-6 lg:my-8 dark:text-white md:text-5xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
-        {text}&nbsp;
+    <header className="flex flex-col items-center lg:items-start md:gap-6">
+      <h1 className="flex flex-wrap my-6 text-4xl font-bold text-gray-800 md:my-6 lg:my-4 dark:text-white md:text-5xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
+        {text}
         <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-500">
+          {text && <span>&nbsp;</span>}
           {highlightedText}
         </span>
         {afterHighlighted}
       </h1>
       {caption && (
-        <p className="mb-4 text-gray-600 text-md md:text-center md:text-base md:mb-0 xl:text-xl 2xl:text-2xl">
+        <p className="w-3/4 mb-4 text-gray-600 text-md md:text-base md:mb-0 xl:text-xl 2xl:text-2xl">
           {caption}
         </p>
       )}
       {caption2 && (
-        <p className="mb-4 text-gray-600 text-md md:text-center md:text-base md:mb-0 xl:text-xl 2xl:text-2xl">
+        <p className="w-3/4 mb-4 text-gray-600 text-md md:text-base md:mb-0 xl:text-xl 2xl:text-2xl">
           {caption2}
         </p>
       )}

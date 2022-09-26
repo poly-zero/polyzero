d605f49a10e1e6ec25fd102e321c22797d4d9e47
Felipe Machado <felipemfm@gmail.com>
2022-09-26 09:22:58 +0900

    Feat: Split stripeCheckout function into prod and dev

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,11 @@
 const functions = require("firebase-functions");
-const stripe = require("stripe")(functions.config().stripe.key);
+const stripeDev = require("stripe")(functions.config().stripe.dev.key);
+const stripeProd = require("stripe")(functions.config().stripe.prod.key);
 
-exports.stripeCheckout = functions.https.onCall(async (data, context) => {
+exports.stripeCheckoutDev = functions.https.onCall(async (data, context) => {
   const {title, cost, image, idempotencyKey} = data;
 
-  const session = await stripe.checkout.sessions.create(
+  const session = await stripeDev.checkout.sessions.create(
       {
         payment_method_types: ["card"],
         line_items: [
@@ -23,6 +24,39 @@ exports.stripeCheckout = functions.https.onCall(async (data, context) => {
         mode: "payment",
         success_url: "http://localhost:3000/confirmation",
         cancel_url: "http://localhost:3000/payment",
+      },
+      {
+        idempotencyKey: idempotencyKey,
+      },
+  );
+  const res = session;
+  functions.logger.log(res);
+  return res;
+});
+
+
+exports.stripeCheckoutProd = functions.https.onCall(async (data, context) => {
+  const {title, cost, image, idempotencyKey} = data;
+
+  const session = await stripeProd.checkout.sessions.create(
+      {
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            price_data: {
+              currency: "jpy",
+              product_data: {
+                name: title,
+                images: [image],
+              },
+              unit_amount: cost,
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: "https://polyzero.earth/confirmation",
+        cancel_url: "https://polyzero.earth/payment",
       },
       {
         idempotencyKey: idempotencyKey,

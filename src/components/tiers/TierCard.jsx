9cd751eef8ef0c284d5930d70e82c47cda529f88
Kim <kimly.jet@gmail.com>
2022-09-26 14:09:33 +0900

    Feat: Use carousel for tier cards

--- src/components/tiers/TierCard.jsx
+++ src/components/tiers/TierCard.jsx
@@ -29,65 +29,67 @@ const TierCard = ({ title, time, tonnes, cost, image, setTier }) => {
   }
 
   return (
-    <article className="basis-1/4">
-      <Card className="transition duration-300 ease-out hover:-translate-y-1 hover:scale-105">
-        <img className="w-full rounded-t-xl" src={image} alt="" />
-        <CardBody className="flex flex-col gap-2">
-          <h2 className="font-bold tracking-tight text-gray-800 lg:text-2xl dark:text-white">
-            {title}
-          </h2>
-          <p className="font-normal text-gray-700 whitespace-pre-wrap text-md dark:text-gray-400">
-            {title === "Champion" ? (
-              <span>
-                Offset for a <strong>lifetime</strong>
-              </span>
-            ) : (
-              <span>
-                Off set for <strong>{time} year(s)</strong>
-              </span>
+    <article className="basis-2/2">
+      <Card className="my-4 transition duration-300 ease-out hover:-translate-y-1 hover:scale-105">
+        <div className="flex">
+          <img className="w-2/4 rounded-l-xl" src={image} alt="" />
+          <CardBody className="flex flex-col w-3/4 gap-4">
+            <h2 className="font-bold tracking-tight text-gray-800 lg:text-2xl dark:text-white">
+              {title}
+            </h2>
+            <p className="font-normal text-gray-700 whitespace-pre-wrap text-md dark:text-gray-400">
+              {title === "Champion" ? (
+                <span>
+                  Offset for a <strong>lifetime</strong>
+                </span>
+              ) : (
+                <span>
+                  Off set for <strong>{time} year(s)</strong>
+                </span>
+              )}
+            </p>
+            {title === "Champion" && (
+              <div>
+                <Label htmlFor="age1" value="Enter number of years" />
+                <TextInput
+                  id="age1"
+                  type="number"
+                  placeholder="Number of years to offset"
+                  min={15}
+                  max={100}
+                  value={age}
+                  onChange={(e) => setAge(e.target.value)}
+                  required={true}
+                />
+              </div>
             )}
-          </p>
-          {title === "Champion" && (
-            <div>
-              <Label htmlFor="age1" value="Enter number of years" />
-              <TextInput
-                id="age1"
-                type="number"
-                placeholder="Number of years to offset"
-                min={15}
-                max={100}
-                value={age}
-                onChange={(e) => setAge(e.target.value)}
-                required={true}
-              />
-            </div>
-          )}
-          {age >= time ? (
-            <>
-              <p className="text-gray-700 text-md dark:text-gray-400">
-                Total CO2:{" "}
-                <span className="font-bold">{` ${(tonnes * age).toFixed(
-                  2
-                )} tonnes`}</span>
-              </p>
-              <h3 className="font-bold tracking-tight text-gray-800 lg:text-2xl dark:text-white">
-                {`￥${(cost * age).toLocaleString("ja-JP")}`}
-              </h3>
-              <Button
-                className="capitalize text-md hover:bg-blue-600 focus:ring-4 dark:bg-blue-600 dark:hover:bg-blue-700"
-                onClick={assignTier}
-              >
-                Select
-              </Button>
-            </>
-          ) : (
-            <>
-              <p className="font-bold text-red-500 text-md dark:text-gray-400">
-                Please enter a number of year equal or above 15.
-              </p>
-            </>
-          )}
-        </CardBody>
+            <p className="text-gray-700 text-md dark:text-gray-400">
+              Total CO2:{" "}
+              <span className="font-bold">{` ${(tonnes * age).toFixed(
+                2
+              )} tonnes`}</span>
+            </p>
+            <h3 className="font-bold tracking-tight text-gray-800 lg:text-2xl dark:text-white">
+              {`￥${(cost * age).toLocaleString("ja-JP")}`}
+            </h3>
+            {age >= time ? (
+              <>
+                <Button
+                  className="my-auto capitalize text-md hover:bg-blue-600 focus:ring-4 dark:bg-blue-600 dark:hover:bg-blue-700"
+                  onClick={assignTier}
+                >
+                  Select
+                </Button>
+              </>
+            ) : (
+              <>
+                <p className="font-bold text-red-500 text-md dark:text-gray-400">
+                  Please enter a number of year equal or above 15.
+                </p>
+              </>
+            )}
+          </CardBody>
+        </div>
       </Card>
     </article>
   );

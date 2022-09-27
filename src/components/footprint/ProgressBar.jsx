41bcefc4100b9fbb95688c22f9acc160406cdf5d
Kim <kimly.jet@gmail.com>
2022-09-27 11:57:45 +0900

    Refactor:  Move conditions outside of attribute

--- src/components/footprint/ProgressBar.jsx
+++ src/components/footprint/ProgressBar.jsx
@@ -1,29 +1,28 @@
 import { Progress } from "@material-tailwind/react";
-import { useCountUp } from "use-count-up";
 
 const ProgressBar = ({ useWizard }) => {
   const { isLastStep, isFirstStep, activeStep } = useWizard();
 
+  const value = isFirstStep
+    ? 0
+    : activeStep === 1
+    ? 20
+    : activeStep === 2
+    ? 40
+    : activeStep === 3
+    ? 60
+    : activeStep === 4
+    ? 80
+    : isLastStep
+    ? 100
+    : null;
+
   return (
     <>
       <Progress
-        value={
-          isFirstStep
-            ? 0
-            : activeStep === 1
-            ? 20
-            : activeStep === 2
-            ? 40
-            : activeStep === 3
-            ? 60
-            : activeStep === 4
-            ? 80
-            : isLastStep
-            ? 100
-            : ""
-          }
-          label="Completed"
-          color="green"
+        value={value}
+        label="Completed"
+        color="green"
         className="rounded-none"
       />
     </>

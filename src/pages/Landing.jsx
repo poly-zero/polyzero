fe2ffca2ae236dceb01865e4dd6341ac46708622
Felipe Machado <felipemfm@gmail.com>
2022-09-27 09:09:51 +0900

    Fix: Attribution links now open in a new tab

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -40,6 +40,8 @@ const Landing = () => {
           Photo by{" "}
           <a
             className="underline"
+            target="_blank"
+            rel="noreferrer"
             href="https://unsplash.com/@flyd2069?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
           >
             FLY:D
@@ -48,6 +50,8 @@ const Landing = () => {
           <a
             className="underline"
             href="https://unsplash.com/s/photos/plastic-straws?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            target="_blank"
+            rel="noreferrer"
           >
             Unsplash
           </a>
